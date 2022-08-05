a86db26946e08af4babf837c2cc82db6af91cb49
modithapahan <modithapahan0@gmail.com>
2022-08-05 11:46:34 +0530

    Backend

--- routes/details.js
+++ routes/details.js
@@ -4,7 +4,7 @@ const detail = require('../models/detail');
 router.route('/add').post(async(req,res)=>{
     const {name,age,gender,email,city,salary} = req.body;
     const addDetals = {name,age,gender,email,city,salary};
-    const details = new detail(addDetals)
+    const details = new detail(addDetals);
 
     await details.save().then(()=>{
         res.status(200).send({status:"Added Details Successfully!"});
@@ -19,6 +19,32 @@ router.route('/:id').get(async(req,res)=>{
 
     await detail.findById(userId).then((data)=>{
         res.status(200).send(data);
+    }).catch((error)=>{
+        console.log(error);
+        res.status(404).send({status:"Error"});
+    })
+})
+
+router.route('/update/:id').put(async (req,res)=>{
+    const userId = req.params.id;
+
+    const {name,age,gender,email,city,salary} = req.body;
+    const updateDetails = {name,age,gender,email,city,salary}
+    const details = new detail(updateDetails);
+
+    await detail.findByIdAndUpdate(userId,details).then((data)=>{
+        res.status(200).send({status:"Details updated Successfully!"},data);
+    }).catch((error)=>{
+        console.log(error);
+        res.status(404).send({status:"Error"});
+    })
+})
+
+router.route('/delete/:id').delete(async (req,res)=>{
+    const userId = req.params.id;
+
+    await detail.findByIdAndDelete(userId).then(()=>{
+        res.status(200).send({status:"Details deleted Successfully!"});
     }).catch((error)=>{
         console.log(error);
         res.status(404).send({status:"Error"});
