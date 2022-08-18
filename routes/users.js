e0773778055519afa0dcf82762c2fc9d9e6146c7
modithapahan <modithapahan0@gmail.com>
2022-08-18 12:37:06 +0530

    Employee Management System

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,11 @@ router.post('/register', async(req,res) => {
 
       const {error} = registerValidate(req.body);
       if(error) return res.status(400).send(error.details[0].message);
-    
+
       const emailExist = await User.findOne({
         email: req.body.email
       })
-      if(emailExist){
-        return res.status(400).send('Email already exist')
-      };
+      if(emailExist) return res.status(400).send('Email already exist');
 
       const salt = await bcrypt.genSalt(10);
       const hashPassword = await bcrypt.hash(req.body.password, salt);
@@ -27,26 +25,28 @@ router.post('/register', async(req,res) => {
 
       try {
         const saveUser = await user.save();
-        res.status(200).send({user: saveUser._id});
-      } catch (error) {
+        res.status(200).send({user: saveUser.id});
+      } 
+      catch (error) {
         res.status(200).send(error);
       }
+
 });
 
-router.post('/login', async(req,res) => {
+// router.post('/login', async(req,res) => {
 
-      const {error} = loginValidate(req.data);
-      if(error) return res.status(400).send(error.details[0].message);
+//       const {error} = loginValidate(req.data);
+//       if(error) return res.status(400).send(error.details[0].message);
     
-      const user = await User.findOne({email: req.body.email});
-      if(!user) return res.status(400).send('Invalid Email');
+//       const user = await User.findOne({email: req.body.email});
+//       if(!user) return res.status(400).send('Invalid Email');
 
-      const validPassword = await bcrypt.compare(req.body.password,user.password);
-      if(!validPassword) return res.status(400).send('Invalid Password!');
+//       const validPassword = await bcrypt.compare(req.body.password,user.password);
+//       if(!validPassword) return res.status(400).send('Invalid Password!');
 
-      /* Generate a token */
-      const token = jwt.sign({_id: user._id}, process.senv.TOKEN_SECRET);
-      res.header('auth-token',token).send(token);
-});
+//       /* Generate a token */
+//       const token = jwt.sign({_id: user._id}, process.senv.TOKEN_SECRET);
+//       res.header('auth-token',token).send(token);
+// });
 
 module.exports = router;
